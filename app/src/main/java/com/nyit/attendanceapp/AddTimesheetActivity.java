f6718bc4c1aa74688dcc66ca33caca67e443ef89
Stefan Teau <50839452+StefanTeau7@users.noreply.github.com>
2020-04-15 16:22:28 +0300

    1st commit

--- app/src/main/java/com/nyit/attendanceapp/AddTimesheetActivity.java
+++ app/src/main/java/com/nyit/attendanceapp/AddTimesheetActivity.java
@@ -1,4 +1,5 @@
 package com.nyit.attendanceapp;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -6,16 +7,17 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 
-public class AddStudentsActivity extends Activity {
+public class AddTimesheetActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_student_dialog);
+        setContentView(R.layout.create_timesheet_dialog);
         configureBackButton();
+
     }
 
     private void configureBackButton(){
-        AppCompatButton acb = findViewById(R.id.cancelStudents);
+        AppCompatButton acb = findViewById(R.id.cancelSheets);
         acb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +25,5 @@ public class AddStudentsActivity extends Activity {
             }
         });
     }
-
-
 
 }
--- app/src/main/java/com/nyit/attendanceapp/SheetsFragement.java
+++ app/src/main/java/com/nyit/attendanceapp/SheetsFragement.java
@@ -1,5 +1,6 @@
 package com.nyit.attendanceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SheetsFragement extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class SheetsFragement extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sheets,container,false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        FloatingActionButton fab = getActivity().findViewById(R.id.cancel);
+        fab.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getActivity(), AddStudentsActivity.class);
+        getActivity().startActivity(intent);
     }
 }
