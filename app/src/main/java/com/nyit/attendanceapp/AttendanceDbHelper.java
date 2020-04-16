8ddbbc476f9b4682dfb644c74bb6a565919da02e
Luke Harrison <35813094+lharrison1313@users.noreply.github.com>
2020-04-16 12:04:17 -0400

    fixed class

--- app/src/main/java/com/nyit/attendanceapp/AttendanceDbHelper.java
+++ app/src/main/java/com/nyit/attendanceapp/AttendanceDbHelper.java
@@ -85,4 +85,6 @@ public class AttendanceDbHelper extends SQLiteOpenHelper {
 
 
 
+
+
 }
--- app/src/main/java/com/nyit/attendanceapp/Class.java
+++ app/src/main/java/com/nyit/attendanceapp/Class.java
@@ -3,15 +3,19 @@ package com.nyit.attendanceapp;
 public class Class {
     private String name;
     private String section;
+    private String cid;
 
-    public Class (String name, String section){
+    public Class (String name, String section, String cid){
         this.name = name;
         this.section = section;
+        this.cid = cid;
     }
 
     public String getName() {return name;}
 
     public String getSection() {return section;}
+
+    public String getCid() {return cid;}
 
 
 }
