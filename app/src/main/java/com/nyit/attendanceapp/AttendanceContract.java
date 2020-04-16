ac72204d2c313baa8c35c7c3cf72401d32689996
Luke Harrison <35813094+lharrison1313@users.noreply.github.com>
2020-04-16 12:05:07 -0400

    udate attendance contract

--- app/src/main/java/com/nyit/attendanceapp/AttendanceContract.java
+++ app/src/main/java/com/nyit/attendanceapp/AttendanceContract.java
@@ -15,27 +15,4 @@ public class AttendanceContract {
 
     }
 
-//    public static class ClassTable{
-//        //primary key CID and Section
-//        public static final String TABLE_NAME = "Class";
-//        public static final String COLUMN_NAME_CID = "CID";
-//        public static final String COLUMN_NAME_CNAME = "cName";
-//        public static final String COLUMN_NAME_SECTION = "Section";
-//    }
-//
-//    public static class StudentTakesTable{
-//        public static final String TABLE_NAME = "Student_Takes";
-//        public static final String COLUMN_NAME_SID = "SID";
-//        public static final String COLUMN_NAME_CID = "CID";
-//        public static final String COLUMN_NAME_SECTION = "Section";
-//    }
-//
-//    public static class SheetTable{
-//        public static final String TABLE_NAME = "Sheet";
-//        public static final String COLUMN_NAME_SHEETID = "SheetID";
-//        public static final String COLUMN_NAME_Date = "Date";
-//        public static final String COLUMN_NAME_CID = "CID";
-//        public static final String COLUMN_NAME_SECTION = "Section";
-//    }
-
 }
